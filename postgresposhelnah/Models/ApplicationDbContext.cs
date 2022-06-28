d7ef13c204a0426f3d182b06a537c993ccedc372
Denis le <denis.le01@mail.ru>
2022-06-28 22:39:24 +0300

    Кнопка Denis

---
+++ postgresposhelnah/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using postgresposhelnah.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace postgresposhelnah.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        DbSet<books> books { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ postgresposhelnah/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace postgresposhelnah.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
